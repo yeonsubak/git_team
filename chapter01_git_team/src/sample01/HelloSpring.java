44dcfb8de755dd07e237855a0315054fe592024e
yeonsubak <yeonsubak@gmail.com>
2022-06-17 12:44:09 +0900

    yb

--- chapter01_git_team/src/sample01/HelloSpring.java
+++ chapter01_git_team/src/sample01/HelloSpring.java
@@ -6,12 +6,13 @@ public class HelloSpring {
 		MessageBean messageBean = new MessageBean();
 		messageBean.sayHello("Spting");
 		
-		System.out.println("깃 연결 성공");
-		System.out.println("박연수: commit conflict 테스트");
-		System.out.println("이원형: 오늘 점심은 생선까스");
-		System.out.println("박연수: 배고픕니다.");
-		
+		HelloSpring helloSpring = new HelloSpring();
+		helloSpring.yeonsu();
 		
 	}
 
+	public void yeonsu() {
+		System.out.println("박연수입니다.");
+	}
+	
 }
